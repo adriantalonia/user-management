2cb199904896c11c9d6cbec45dc676b30496b026
Adrian <adrian.talonia11@gmail.com>
2024-03-05 22:46:58 -0600

    add search users query structure

--- um-graphql-postgres-maven/src/main/java/org/umgpm/content/graphql/UserGraphQLController.java
+++ um-graphql-postgres-maven/src/main/java/org/umgpm/content/graphql/UserGraphQLController.java
@@ -5,8 +5,12 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import org.umgpm.content.codegen.DgsConstants;
+import org.umgpm.content.codegen.types.SearchFilterInput;
 import org.umgpm.content.codegen.types.User;
 import org.umgpm.content.service.UserService;
+
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,5 +22,10 @@ public class UserGraphQLController {
     public User getUser(@Argument String id) {
         userService.getUserById(id);
         return User.newBuilder().id(id).firstName("test").build();
+    }
+
+    @QueryMapping(name = DgsConstants.QUERY.SearchUsers)
+    public List<User> searchUsers(@Argument SearchFilterInput input) {
+        return Collections.emptyList();
     }
 }
